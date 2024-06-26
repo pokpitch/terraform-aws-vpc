5dcb5990e834f0b57248ba9921a88a769a667a25
Pokpitch P <pokpitch@gmail.com>
2024-06-26 20:45:22 +0700

    update: region and az

--- variables.tf
+++ variables.tf
@@ -10,9 +10,9 @@ variable "networking" {
   })
   default = {
     cidr_block      = "10.0.0.0/16"
-    region          = "us-east-1"
+    region          = "ap-southeast-2"
     vpc_name        = "custom-vpc"
-    azs             = ["us-east-1a", "us-east-1b"]
+    azs             = ["ap-southeast-2a", "ap-southeast-2b"]
     public_subnets  = ["10.0.1.0/24", "10.0.2.0/24"]
     private_subnets = ["10.0.3.0/24", "10.0.4.0/24"]
     nat_gateways    = true
